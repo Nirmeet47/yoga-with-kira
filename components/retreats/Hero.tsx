c17b25fdd21971a5a3767de8f74b2a5b30f0d218
Nirmeet <nirmeetparmar21@gmail.com>
2025-08-11 11:46:26 +0530

    responsiveness fixed for retreats hero img

--- components/retreats/Hero.tsx
+++ components/retreats/Hero.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <section className="bg-[#f7f7f7] h-[calc(100vh-80px)] overflow-hidden">
-      <div className="h-full max-w-7xl mx-auto flex flex-col px-4 sm:px-6 lg:px-8 xl:px-12">
+    <section className="bg-[#f7f7f7]">
+      <div className="max-w-7xl mx-auto flex flex-col px-4 sm:px-6 lg:px-8 xl:px-12 py-4 sm:py-6 lg:py-8">
         
-        {/* Header Section - Fixed height */}
+        {/* Header Section */}
         <div className="text-center pt-8 pb-6">
           <h1 className="font-amaranth text-3xl md:text-4xl lg:text-5xl font-light leading-tight text-gray-900 mb-3">
             <span className="text-[#A9D941]">Retreats</span> That Move You
@@ -21,15 +21,15 @@ const Hero: React.FC = () => {
           </p>
         </div>
 
-        {/* Main Image Section - Takes all remaining space */}
-        <div className="flex-1 relative rounded-xl overflow-hidden mb-8">
+        {/* Main Image - Fixed responsiveness */}
+        <div className="relative w-full h-40 sm:h-56 md:h-72 lg:h-80 xl:h-[23rem] rounded-xl overflow-hidden mb-8">
           <Image
             src="/images/retreats/retreats-hero.jpg"
             alt="Yoga retreat in mountains with three women practicing warrior pose"
             fill
             className="object-cover object-center"
             priority
-            sizes="100vw"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 80vw"
           />
         </div>
       </div>
